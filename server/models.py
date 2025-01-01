7b85167a17aa3edf26831f65a10a440579f37fb8
musin2 <muhsindaud75@gmail.com>
2025-01-02 00:35:00 +0300

    adds model relationships

--- server/models.py
+++ server/models.py
@@ -5,46 +5,63 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-# [ ] MODELS
+
+# [x] MODELS
 class UserRole(Enum):
     admin = "admin"
     employee = "employee"
 
+
 class BillType(Enum):
-    medical = 'Medical'
-    surgical = 'Surgical'
+    medical = "Medical"
+    surgical = "Surgical"
+
 
 class OrganizationType(Enum):
-    insurance = 'Insurance Company'
-    association = 'Association'
-    corporation = 'Company / Corporation'
+    insurance = "Insurance Company"
+    association = "Association"
+    corporation = "Company / Corporation"
+
 
 class User(SerializerMixin):
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(255), nullable = False)
-    user_role = db.Column(db.Enum(UserRole), nullable = False)
-    email = db.Column(db.String(255), nullable = False, unique = True)
-    password = db.Column(db.String(200), nullable = False)
+    user_name = db.Column(db.String(255), nullable=False)
+    user_role = db.Column(db.Enum(UserRole), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
 
 class PatientBill(SerializerMixin):
-    __tablename__ = 'patient_bills'
+    __tablename__ = "patient_bills"
 
     patient_id = db.Column(db.Integer, primary_key=True)
-    patient_name = db.Column(db.String(255), nullable = False)
-    bill_date = db.Column(db.DateTime, nullable = False)
-    organization_id = db.Column(db.Integer, db.ForeignKey('organizations.org_id'), nullable=False)
-    bill_type = db.Column(db.Enum(BillType), nullable = False)
-    amount = db.Column (db.Integer)
-    created_at = db.Column(db.DateTime, default = datetime.now().astimezone())
+    patient_name = db.Column(db.String(255), nullable=False)
+    bill_date = db.Column(db.DateTime, nullable=False)
+    organization_id = db.Column(
+        db.Integer, db.ForeignKey("organizations.org_id"), nullable=False
+    )
+    bill_type = db.Column(db.Enum(BillType), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now().astimezone())
     updated_at = db.Column(db.Datetime)
 
+    org = db.relationship('Organization', back_populates = 'bills', cascade = "all, delete-orphan")
+
+    serialize_rules = ("-org.bills")
+
+
 class Organization(SerializerMixin):
-    __tablename__ = 'organizations'
+    __tablename__ = "organizations"
 
-    org_id = db.Column(db.Integer, primary_key = True)
-    org_name = db.Column(db.String(255), nullable = False)
-    org_type = db.Column(db.Enum(OrganizationType), nullable = False)
+    org_id = db.Column(db.Integer, primary_key=True)
+    org_name = db.Column(db.String(255), nullable=False)
+    org_type = db.Column(db.Enum(OrganizationType), nullable=False)
 
-# [ ] RELATIONSHIPS
+    bills = db.relationship('PatientBill', back_populates = 'org', cascade = "all, delete-orphan")
+
+    serialize_rules = ("-bills.org")
+
+
+# [x] RELATIONSHIPS
