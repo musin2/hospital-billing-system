3f6c3f4ba40c1708b3da4da5eb07ee3b79b068b0
musin2 <muhsindaud75@gmail.com>
2025-03-26 17:35:57 +0300

    modifies validation decorator

--- server/app.py
+++ server/app.py
@@ -32,6 +32,44 @@ db.init_app(app)
 # [ ] User authentication using PYJWT
 
 
+def check_record(object):
+    if object is None:
+        return make_response({"error":"Record not found"},404)
+    if not isinstance(object,(Organization,User,Transaction,AuditLog,PatientBill,Adjustment,PaidBill,VoidBill)):
+        return make_response({"error":"Invalid record type"},404)
+    # Check if record is voided
+    if hasattr(object,"voided_at") and object.voided_at is not None:
+        return make_response({"error":"Record is  invalid (void)"},400)
+    return None
+
+# Decorator function for validation when looking up records with URL parameter
+def validation(funct):
+	@wraps(funct)
+	def wrapper(*args,**kwargs):
+		id = kwargs.get('id')
+		table = kwargs.get('table')
+		record = None
+		
+		if id is None or not isinstance(id, int) or id < 0:
+			return make_response({"error":f"Invalid {table} ID"},400)
+			
+		if table == "Organization":
+			record =  Organization.query.filter_by(org_id = id).first()	
+		elif table == "User":
+			record = User.query.filter_by(user_id = id).first()
+		elif table == "Transaction":
+			record = Transaction.query.filter_by(transaction_id = id).first()
+		else:
+			return make_response({"error":"Invalid Table"},400)
+		error_response = check_record(record)
+		
+		if error_response:
+			return error_response
+		kwargs['record'] = record
+		return funct(*args, **kwargs)
+	return wrapper
+
+# [ ] Modify functions that use validation decorator with table_name & all url parameters = id
 # Get, Patch, & Delete a specific user
 class UserAPI(Resource):
     # Decorator funcion for validation
