743cebd33740373d327a19182fb88661a6af9035
musin2 <muhsindaud75@gmail.com>
2025-04-30 19:01:41 +0300

    binds table argument to decorator

--- server/app/utils/decorators.py
+++ server/app/utils/decorators.py
@@ -33,39 +33,40 @@ def check_record(object):
     return None
 
 # Decorator function for validation when looking up records with URL parameter
-def validate_record(funct):
-    @wraps(funct)
-    def wrapper(*args, **kwargs):
-        id = kwargs.get("id")
-        table = kwargs.get("table")
-        record = None
+def validate_record(table):
+    def decorator(funct):
+        @wraps(funct)
+        def wrapper(*args, **kwargs):
+            id = kwargs.get("id")
+            record = None
 
-        if id is None or not isinstance(id, int) or id < 0:
-            return make_response({"error": f"Invalid {table} ID"}, 400)
+            if id is None or not isinstance(id, int) or id < 0:
+                return make_response({"error": f"Invalid {table} ID"}, 400)
 
-        if table == "Organization":
-            record = Organization.query.filter_by(org_id=id).first()
-        elif table == "User":
-            record = User.query.filter_by(user_id=id).first()
-        elif table == "Transaction":
-            record = Transaction.query.filter_by(transaction_id=id).first()
-        elif table == "AuditLog":
-            record = AuditLog.query.filter_by(log_id=id).first()
-        elif table == "PatientBill":
-            record = PatientBill.query.filter_by(bill_id=id).first()
-        elif table == "Adjustment":
-            record = Adjustment.query.filter_by(adjustment_id=id).first()
-        elif table == "PaidBill":
-            record = PaidBill.query.filter_by(bill_id=id).first()
-        elif table == "VoidBill":
-            record = VoidBill.query.filter_by(bill_id=id).first()
-        else:
-            return make_response({"error": "Invalid Table"}, 400)
-        error_response = check_record(record)
+            if table == "Organization":
+                record = Organization.query.filter_by(org_id=id).first()
+            elif table == "User":
+                record = User.query.filter_by(user_id=id).first()
+            elif table == "Transaction":
+                record = Transaction.query.filter_by(transaction_id=id).first()
+            elif table == "AuditLog":
+                record = AuditLog.query.filter_by(log_id=id).first()
+            elif table == "PatientBill":
+                record = PatientBill.query.filter_by(bill_id=id).first()
+            elif table == "Adjustment":
+                record = Adjustment.query.filter_by(adjustment_id=id).first()
+            elif table == "PaidBill":
+                record = PaidBill.query.filter_by(bill_id=id).first()
+            elif table == "VoidBill":
+                record = VoidBill.query.filter_by(bill_id=id).first()
+            else:
+                return make_response({"error": "Invalid Table"}, 400)
+            error_response = check_record(record)
 
-        if error_response:
-            return error_response
-        kwargs["record"] = record
-        return funct(*args, **kwargs)
+            if error_response:
+                return error_response
+            kwargs["record"] = record
+            return funct(*args, **kwargs)
 
-    return wrapper
+        return wrapper
+    return decorator
