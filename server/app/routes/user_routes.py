994d9782d35c211aa95d9eba3734266ff4281cdd
musin2 <muhsindaud75@gmail.com>
2025-04-30 18:59:13 +0300

    adds service functions

--- server/app/routes/user_routes.py
+++ server/app/routes/user_routes.py
@@ -1,30 +1,39 @@
-from flask_restx import Resource
+from flask import Blueprint
+from flask_restx import Resource, Namespace
 from app.utils.decorators import validate_record
 from app.extensions import api
-# Get, Patch, & Delete a specific user
-class UserAPI(Resource):
+from app.services.user_service import get_user_by_id, create_new_user
+from app.api_models.user_models import user_model, create_user_model
 
-    @validate_record
-    def get(self, id, record, table="User"):
-        pass
+user_bp = Blueprint("user", __name__)
+user_ns = Namespace("users", description="User operations", path="/")
+
+
+# Get, Patch, & Delete a specific user
+@user_ns.route("/user/<int:id>")
+class UserAPI(Resource):
+    # @user_ns.marshal_list_with(user_model)
+    @validate_record("User")
+    def get(self, id, record):
+        """List User"""
+        return get_user_by_id(id, record)
 
     # [ ] Fill AuditLog table before applying the changes
     # [ ] Check dynamic setting of attributes (is appropriate / secure ?)
-    @validate_record
-    def patch(self, id, record, table="User"):
+    @validate_record("User")
+    def patch(self, id, record):
         pass
 
-    @validate_record
-    def delete(self, id, record, table="User"):
+    @validate_record("User")
+    def delete(self, id, record):
         pass
-
-
-api.add_resource(UserAPI, "/user/<int:id>")
 
 
 # Create new user
+@user_ns.route("/user")
 class NewUser(Resource):
+    @user_ns.expect(create_user_model, validate=True)
+    @user_ns.marshal_with(user_model, code=201)
     def post(self):
-        pass
-
-api.add_resource(NewUser, "/user")
+        """Create New User"""
+        return create_new_user()
