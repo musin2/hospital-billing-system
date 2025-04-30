d5b2b0a6dfa5df89a2c96dcd8d9964244f3e67bf
musin2 <muhsindaud75@gmail.com>
2025-04-30 18:58:26 +0300

    registers user bp / ns

--- server/app/routes/__init__.py
+++ server/app/routes/__init__.py
@@ -1,6 +1,9 @@
-
+from flask import Flask
+from app.routes.user_routes import user_ns,user_bp
+from app.extensions import api
 
 # [ ] Generate requirements.txt ?
 
-def register_blueprints(app):
-    pass
+def register_blueprints(app:Flask):
+    app.register_blueprint(user_bp)
+    api.add_namespace(user_ns,path="")
