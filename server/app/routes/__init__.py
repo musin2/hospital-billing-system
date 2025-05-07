f2f02328341a146d630527aca0ea5baaaf3581d4
musin2 <muhsindaud75@gmail.com>
2025-05-07 18:41:00 +0300

    registers bill blueprint and namespace

--- server/app/routes/__init__.py
+++ server/app/routes/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.routes.user_routes import user_ns,user_bp
+from app.routes.bill_routes import bill_ns, bill_bp
 from app.extensions import api
 
 # [ ] Generate requirements.txt ?
@@ -7,3 +8,6 @@ from app.extensions import api
 def register_blueprints(app:Flask):
     app.register_blueprint(user_bp)
     api.add_namespace(user_ns,path="")
+
+    app.register_blueprint(bill_bp)
+    api.add_namespace(bill_ns)
