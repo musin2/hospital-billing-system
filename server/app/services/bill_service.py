ea2b29ebe3cac53ff5527cf9cc8e98a811940441
musin2 <muhsindaud75@gmail.com>
2025-05-09 19:17:00 +0300

    feat: create bill function

--- server/app/services/bill_service.py
+++ server/app/services/bill_service.py
@@ -1,6 +1,10 @@
+from flask import make_response, request
+from datetime import datetime
 from app.models.patientbill import PatientBill
-from flask import make_response
 from app.utils.serialization import serialize_bill
+from app.utils.enums import BillStatus
+from app.extensions import db
+
 
 def get_all_bills():
     try:
@@ -14,14 +18,84 @@ def get_all_bills():
         # Return error message with status code 500 if something goes wrong
         return make_response({"error": str(e)}, 500)
 
+
 def create_new_bill():
-    pass
+    try:
+        data = request.get_json()  # Retreive form data
+
+        # Data Validation
+        required_fields = [
+            "patient_number",
+            "patient_id",
+            "patient_name",
+            "patient_gender",
+            "patient_phone_number",
+            "bill_date",
+            "organization_id",
+            "bill_type",
+            "amount",
+        ]
+        # [ ] additional validation in database ? with column data types
+        for field in required_fields:
+            if field not in data:
+                return make_response({"error": f"Missing data: {field}"}, 400)
+
+        # [ ] Calculate age from birthdate / Calculate birthdate from age
+        patient_number = data["patient_number"]
+        patient_id = data["patient_id"]
+        patient_name = data["patient_name"]
+        patient_gender = data["patient_gender"]
+        patient_age = data["patient_age"]
+        patient_birthdate = data["patient_birthdate"]
+        # [ ] phone number format 712356789 from form
+        # add country code
+        # number = country code(select menu??) + form input
+        # [ ] Validate phone number lenght
+        patient_phone_number = data["patient_phone_number"]
+        bill_date = data["bill_date"]
+        organization_id = data["organization_id"]
+        # [ ] Use Enum???
+        bill_type = data["bill_type"]
+        print(bill_type)
+        print(patient_gender)
+        amount = data["amount"]
+        paid_amount = data["paid_amount"]
+        # created_at & updated_at are set to current time at database level (default / onupdate = func.now())
+        
+        if patient_age is None and patient_birthdate is None:
+            return make_response({"error":"Patient's age or birthdate must be included"},400) 
+        new_bill = PatientBill(
+            patient_number=patient_number,
+            patient_id=patient_id,
+            patient_name=patient_name,
+            patient_gender=patient_gender,
+            patient_age=patient_age,
+            patient_birthdate=patient_birthdate,
+            patient_phone_number=patient_phone_number,
+            bill_date=bill_date,
+            organization_id=organization_id,
+            bill_type=bill_type,
+            amount=amount,
+            status=BillStatus.unpaid.value
+        )
+        # [ ] update outstanding_balance in 'Organization' table
+        db.session.add(new_bill)
+        db.session.commit()
+        response_body = {"message": "Bill created successfully!"}
+        return make_response(response_body, 200)
+
+    except Exception as e:
+        db.session.rollback()
+        return make_response({"error": str(e)}, 500)
+
 
 def get_bill_by_id():
     pass
 
+
 def edit_bill():
     pass
+
 
 def void_bill():
     pass
