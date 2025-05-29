9f3d10002c4e1f207765ecbd63ef02ea30153081
musin2 <muhsindaud75@gmail.com>
2025-05-29 18:19:45 +0300

    adds new service function

--- server/app/services/bill_service.py
+++ server/app/services/bill_service.py
@@ -8,6 +8,24 @@ from app.models.organization import Organization
 from app.extensions import db
 from app.utils.validation import validate_and_format_number
 
+# Calculate age / birthdate
+def calculate_age(patient_birthdate):
+    current_date = datetime.now(timezone.utc).date()
+    try:
+        birthdate = datetime.strptime(patient_birthdate, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Invalid birthdate format. Expected YYYY-MM-DD")
+    
+    age_delta = relativedelta(current_date, birthdate)
+    print(f"Calculated age: {age_delta.years}")
+    return age_delta.years
+
+def calculate_birthdate(patient_age):
+    current_year = datetime.now(timezone.utc).year
+    birth_year = current_year - patient_age
+    patient_birthdate = f"{birth_year}-01-01"
+    print(f"Calculated birthdate: {patient_birthdate}")
+    return patient_birthdate
 
 def get_all_bills():
     try:
@@ -53,10 +71,12 @@ def create_new_bill():
         # [ ] phone number format 0712356789 / +254712356789 from frontend
         # [x] Validate phone number
         try:
-            patient_phone_number = validate_and_format_number(data["patient_phone_number"])
+            patient_phone_number = validate_and_format_number(
+                data["patient_phone_number"]
+            )
         except ValueError as e:
-            return make_response({"error":str(e)},400)
-        
+            return make_response({"error": str(e)}, 400)
+
         bill_date = data["bill_date"]
         organization_id = data["organization_id"]
         # [ ] Use Enum???
@@ -74,17 +94,10 @@ def create_new_bill():
 
         # [x] Calculate age from birthdate / Calculate birthdate from age
         if patient_age is not None and patient_birthdate is None:
-            current_year = datetime.now(timezone.utc).year
-            birth_year = current_year - patient_age
-            patient_birthdate = f"{birth_year}-1-1"
-            print(f"Calculated birthdate: {patient_birthdate}")
+            patient_birthdate = calculate_birthdate(patient_age)
 
         if patient_birthdate is not None and patient_age is None:
-            current_date = datetime.now(timezone.utc).date()
-            birthdate = datetime.strptime(patient_birthdate, "%Y-%m-%d")
-            age_delta = relativedelta(current_date,birthdate)
-            patient_age = age_delta.years
-            print(f"Calculated age: {patient_age}")
+            patient_age = calculate_age(patient_birthdate)
 
         new_bill = PatientBill(
             patient_number=patient_number,
@@ -101,7 +114,9 @@ def create_new_bill():
             status=BillStatus.unpaid.value,
         )
         # [ ] update outstanding_balance in 'Organization' table
-        current_organization = Organization.query.filter_by(org_id = organization_id).first()
+        current_organization = Organization.query.filter_by(
+            org_id=organization_id
+        ).first()
         if current_organization is None:
             return make_response({"error": "Organization not found"}, 404)
         current_organization.outstanding_balance += amount
@@ -118,13 +133,57 @@ def create_new_bill():
 
 def get_bill_by_id(record):
     try:
-        return make_response(serialize_bill(record),200)
+        return make_response(serialize_bill(record), 200)
     except Exception as e:
-        return make_response({"error":str(e)},500)
+        return make_response({"error": str(e)}, 500)
 
 
-def edit_bill():
-    pass
+def edit_bill(record):
+    try:
+        # get form data
+        data = request.get_json()
+        # Validation
+        if data is None:
+            return make_response({"error": "Edit data is missing"}, 400)
+
+        restricted_fields = (
+            "bill_id",
+            "amount",
+            "status",
+            "paid_amount",
+            "created_at",
+            "updated_at",
+        )
+
+        for attribute in data:
+            if attribute in restricted_fields:
+                return make_response(
+                    {"error": f"Cannot edit this attribute {attribute}"}, 400
+                )
+            if data[attribute] not in [None,""]:
+                setattr(record, attribute, data[attribute])
+        patient_age = data.get("patient_age")
+        patient_birthdate = data.get("patient_birthdate")
+        if patient_age not in [None, ""] and patient_birthdate in [None, ""]:
+            record.patient_birthdate = calculate_birthdate(patient_age)
+
+        if patient_birthdate not in [None, ""] and patient_age in [None, ""]:
+            record.patient_age = calculate_age(patient_birthdate)
+
+        if data.get("patient_phone_number") is not None:
+            try:
+                patient_phone_number = validate_and_format_number(
+                    data["patient_phone_number"]
+                )
+                record.patient_phone_number = patient_phone_number
+            except ValueError as e:
+                return make_response({"error": str(e)}, 400)
+
+        db.session.commit()
+        return make_response({"message":"Bill has been edited successfully"},200)
+
+    except Exception as e:
+        return make_response({"error": str(e)}, 500)
 
 
 def void_bill():
