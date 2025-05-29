f9edcad22a9c67930cd18c52d8a4f5a6c24c0a6f
musin2 <muhsindaud75@gmail.com>
2025-05-29 18:18:52 +0300

    adds new swagger model

--- server/app/api_models/bill_models.py
+++ server/app/api_models/bill_models.py
@@ -36,3 +36,15 @@ create_patient_bill_model = api.model("PatientBillCreate", {
     "status": fields.String(required=True, enum=[s.value for s in BillStatus]),
     "paid_amount": fields.Float(min=0.0),
 })
+edit_patient_bill_model = api.model("PatientBillCreate", {
+    "patient_number": fields.String(required=True),
+    "patient_id": fields.String(required=True),
+    "patient_name": fields.String(required=True),
+    "patient_gender": fields.String(required=True, enum=[g.value for g in GenderOption]),
+    "patient_age": fields.Integer(required=False, min=0),
+    "patient_birthdate": fields.Date(required=False),
+    "patient_phone_number": fields.String(required=True),
+    "bill_date": fields.Date(required=True),
+    "organization_id": fields.Integer(required=True),
+    "bill_type": fields.String(required=True, enum=[t.value for t in BillType]),
+})
